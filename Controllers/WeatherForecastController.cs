3e3055eb18a7691e82828eab1cfac1ad7622cd10
unknown <qw@VM1234MID1WEB.MID1.PROJ>
2022-07-18 13:18:59 +0000

    (New): Added project to git

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace IDM.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://mid2.proj");
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "mid1", "DC=mid1,DC=proj");
+            UserPrincipal u = new UserPrincipal(ctx);
+            u.GivenName = "Alon";
+            u.Surname = "Arkush";
+            PrincipalSearcher ps = new PrincipalSearcher();
+            ps.QueryFilter = u;
+            PrincipalSearchResult<Principal> psr = ps.FindAll();
+            
+            return Enumerable.Range(1, psr.Count()).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = psr.ElementAt(index - 1).Sid + ""
+            })
+            .ToArray();
+        }
+    }
+}
